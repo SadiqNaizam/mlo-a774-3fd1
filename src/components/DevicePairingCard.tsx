405512f1187b082aea7666105fd29678d8c45d17
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 20:49:13 +0530

    Updated code files: DevicePairingCard.tsx

--- src/components/DevicePairingCard.tsx
+++ src/components/DevicePairingCard.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-// A simple SVG to represent a QR code without needing an external library.
-const QRCodePlaceholder = () => (
-    <svg width="200" height="200" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className="mx-auto bg-white">
-        <path fill="#000" d="M10 10h25v25h-25z m5 5v15h15v-15z M65 10h25v25h-25z m5 5v15h15v-15z M10 65h25v25h-25z m5 5v15h15v-15z M45 10h5v5h-5z M55 10h5v5h-5z M40 15h5v5h-5z M50 20h5v5h-5z M45 25h5v5h-5z M40 30h5v5h-5z M45 35h5v5h-5z M50 35h5v5h-5z M55 35h5v5h-5z M60 35h5v5h-5z M65 35h5v5h-5z M70 35h5v5h-5z M75 35h5v5h-5z M80 35h5v5h-5z M85 35h5v5h-5z M40 40h5v5h-5z M45 45h5v5h-5z M50 50h5v5h-5z M55 55h5v5h-5z M60 60h5v5h-5z M65 65h5v5h-5z M70 70h5v5h-5z M75 75h5v5h-5z M80 80h5v5h-5z M85 85h5v5h-5z M40 60h5v5h-5z M45 65h5v5h-5z M50 70h5v5h-5z M55 75h5v5h-5z M60 80h5v5h-5z M40 85h5v5h-5z" />
-    </svg>
-);
-
 interface DevicePairingCardProps {
+  /**
+   * The full URL to be encoded in the QR code.
+   */
+  pairingUrl: string;
   /**
    * The numerical pin code for manual pairing.
    */
   pairingPin?: string;
 }
 
-const DevicePairingCard: React.FC<DevicePairingCardProps> = ({ pairingPin = "123-456" }) => {
+const DevicePairingCard: React.FC<DevicePairingCardProps> = ({ pairingUrl, pairingPin = "123-456" }) => {
   console.log('DevicePairingCard loaded');
+
+  // Use a public API to generate a QR code from the pairing URL without adding a new dependency.
+  const qrCodeApiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(pairingUrl)}`;
 
   return (
     <Card className="w-full max-w-sm mx-auto shadow-lg">
@@ -28,7 +28,12 @@ const DevicePairingCard: React.FC<DevicePairingCardProps> = ({ pairingPin = "123
       </CardHeader>
       <CardContent className="flex flex-col items-center gap-6 p-6">
         <div className="p-4 bg-white border-4 border-gray-200 rounded-xl">
-          <QRCodePlaceholder />
+          <img 
+            src={qrCodeApiUrl} 
+            alt="Device Pairing QR Code"
+            width={200}
+            height={200}
+          />
         </div>
         
         <div className="text-center w-full">
